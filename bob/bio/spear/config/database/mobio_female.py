4fe31ccb871688c12adef63ba0ec86b0773032cf
Elie KHOURY <elie.khoury@idiap.ch>
2015-07-06 23:06:34 +0200

    minor improvements

--- bob/bio/spear/config/database/mobio_female.py
+++ bob/bio/spear/config/database/mobio_female.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python
 
-import bob.db.mobio
+import bob.db.verification.filelist
 import bob.bio.base
 
-#mobio_wav_directory = "[YOUR_MOBIO_WAV_DIRECTORY]"
-mobio_wav_directory = '/idiap/resource/database/mobio/denoisedAUDIO_16k'
-
-database = bob.bio.base.database.DatabaseBobZT(
-    database = bob.db.mobio.Database(
+mobio_wav_directory = "[YOUR_MOBIO_WAV_DIRECTORY]"
+database = bob.bio.base.database.DatabaseBob(
+    database = bob.db.verification.filelist.Database('bob/bio/spear/config/database/mobio/',
         original_directory = mobio_wav_directory,
         original_extension = ".wav",
     ),
-    name = "mobio-female",
-    protocol = 'female',
-    models_depend_on_protocol = True,
-
-    all_files_options = { 'gender' : 'female' },
-    extractor_training_options = { 'gender' : 'female' },
-    projector_training_options = { 'gender' : 'female' },
-    enroller_training_options = { 'gender' : 'female' },
-    z_probe_options = { 'gender' : 'female' }
+    name = "mobio",
+    protocol = 'mobile0-female',
+    
 )
--- bob/bio/spear/config/database/mobio_male.py
+++ bob/bio/spear/config/database/mobio_male.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 
-import bob.db.mobio
+import bob.db.verification.filelist
 import bob.bio.base
 
-#mobio_wav_directory = "[YOUR_MOBIO_WAV_DIRECTORY]"
-mobio_wav_directory = '/idiap/resource/database/mobio/denoisedAUDIO_16k'
-database = bob.bio.base.database.DatabaseBobZT(
-    database = bob.db.mobio.Database(
+mobio_wav_directory = "[YOUR_MOBIO_WAV_DIRECTORY]"
+
+database = bob.bio.base.database.DatabaseBob(
+    database = bob.db.verification.filelist.Database('bob/bio/spear/config/database/mobio/',
         original_directory = mobio_wav_directory,
         original_extension = ".wav",
     ),
-    name = "mobio-male",
-    protocol = 'male',
-    models_depend_on_protocol = True,
-
-    all_files_options = { 'gender' : 'male' },
-    extractor_training_options = { 'gender' : 'male' },
-    projector_training_options = { 'gender' : 'male' },
-    enroller_training_options = { 'gender' : 'male' },
-    z_probe_options = { 'gender' : 'male' }
+    name = "mobio",
+    protocol = 'mobile0-male',
+    
 )
+
