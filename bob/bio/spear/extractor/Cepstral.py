0e0ee467edcc8b18e65223206f6965f019b473f1
Elie KHOURY <elie.khoury@idiap.ch>
2015-07-06 19:08:53 +0200

    fixing a bug in Cepstral when no speech is encoutered

--- bob/bio/spear/extractor/Cepstral.py
+++ bob/bio/spear/extractor/Cepstral.py
@@ -164,7 +164,7 @@ class Cepstral(Extractor):
     else:
       normalized_features = filtered_features
     if normalized_features.shape[0] == 0:
-      logger.warn("No speech found in: %s", input_file)
+      logger.warn("No speech found for this utterance")
       # But do not keep it empty!!! This avoids errors in next steps
       normalized_features=numpy.array([numpy.zeros(len(features_mask))])
     return normalized_features
