fc8c71b454e89dd51668ee4b9e0ae9794aa2fd25
Amir Mohammadi <183.amir@gmail.com>
2016-03-15 11:35:47 +0100

    mobio-audio now uses the bob.db.mobio interface

--- bob/bio/spear/config/database/mobio_female.py
+++ bob/bio/spear/config/database/mobio_female.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
-import pkg_resources
-import bob.db.verification.filelist
 import bob.bio.base
+import bob.db.mobio
 
 mobio_wav_directory = "[YOUR_MOBIO_WAV_DIRECTORY]"
 
-database = bob.bio.base.database.DatabaseBob(
-    database = bob.db.verification.filelist.Database(pkg_resources.resource_filename('bob.bio.spear', 'config/database/mobio/mobile0-female'),
-        original_directory = mobio_wav_directory,
-        original_extension = ".wav",
+database = bob.bio.base.database.DatabaseBobZT(
+    database=bob.db.mobio.Database(
+        original_directory=mobio_wav_directory,
+        original_extension=".wav",
     ),
-    name = "mobile0-female",
-    protocol = '',
+    name="mobio-female",
+    protocol='female',
+    models_depend_on_protocol=True,
 
+    all_files_options={'gender': 'female'},
+    extractor_training_options={'gender': 'female'},
+    projector_training_options={'gender': 'female'},
+    enroller_training_options={'gender': 'female'},
+    z_probe_options={'gender': 'female'}
 )
