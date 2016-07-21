c9388de8a9e1d2cfe0e74256a9e420cb88882c40
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2016-07-21 16:16:50 +0200

    Refactoring 2016

--- bob/bio/spear/config/database/voxforge.py
+++ bob/bio/spear/config/database/voxforge.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 
-import bob.db.voxforge
-import bob.bio.base
+import bob.bio.db
 
-voxforge_wav_directory = "[YOUR_VOXFORGE_WAV_DIRECTORY]"
+voxforge_wav_directory = "/idiap/resource/database/VoxForge/audio/original/Trunk/Audio/Main/16kHz_16bit"
 
-database = bob.bio.base.database.DatabaseBob(
-    database = bob.db.voxforge.Database(
-        original_directory = voxforge_wav_directory,
-        original_extension = ".wav",
-    ),
-    name = "voxforge",
-    protocol = '', # since no protocol is defined
+database = bob.bio.db.VoxforgeBioDatabase(
+    original_directory=voxforge_wav_directory,
+    original_extension=".wav",
 )
