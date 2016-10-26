2a97e16f57d582d9325d5bc34c0ab2bd094f81b3
Manuel Gunther <siebenkopf@googlemail.com>
2016-10-26 17:41:28 -0600

    Use the new FileListBioDatabase for banca and timit

--- bob/bio/spear/config/database/banca_audio_G.py
+++ bob/bio/spear/config/database/banca_audio_G.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
 import pkg_resources
-import bob.db.bio_filelist
+import bob.bio.base
 
 banca_wav_directory = "[YOUR_BANCA_WAV_DIRECTORY]"
 
-database = bob.db.bio_filelist.Database(pkg_resources.resource_filename('bob.bio.spear', 'config/database/banca'),
-                                        original_directory=banca_wav_directory,
-                                        original_extension=".wav")
+database = bob.bio.base.database.FileListBioDatabase(
+  base_dir = pkg_resources.resource_filename('bob.bio.spear', 'config/database/banca'),
+  original_directory = banca_wav_directory,
+  original_extension = ".wav",
+  name = "banca",
+  protocol = "G"
+)
--- bob/bio/spear/config/database/timit.py
+++ bob/bio/spear/config/database/timit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
 import pkg_resources
-import bob.db.bio_filelist
+import bob.bio.base
 
 timit_wav_directory = "[YOUR_TIMIT_WAV_DIRECTORY]"
 
-database = bob.db.bio_filelist.Database(pkg_resources.resource_filename('bob.bio.spear', 'config/database/timit'),
-                                        original_directory=timit_wav_directory,
-                                        original_extension=".wav")
+database = bob.bio.base.database.FileListBioDatabase(
+  base_dir = pkg_resources.resource_filename('bob.bio.spear', 'config/database/timit'),
+  original_directory = timit_wav_directory,
+  original_extension = ".wav",
+  name = "timit",
+  protocol = "2"
+)
