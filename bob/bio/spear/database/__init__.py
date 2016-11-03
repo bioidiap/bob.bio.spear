335ccabbfcb1f95ee5518fb6b8667923dd775812
Marc Ferras <marc.ferras@idiap.ch>
2016-11-03 11:49:06 +0100

    nist_sre08 training_files and arrange_by_client

--- bob/bio/spear/database/__init__.py
+++ bob/bio/spear/database/__init__.py
@@ -27,6 +27,7 @@ from .avspoof import AVspoofBioDatabase
 from .voicepa import VoicePABioDatabase
 from .nist_sre12 import NistSre12BioDatabase
 from .nist_sre10 import NistSre10BioDatabase
+from .nist_sre08 import NistSre08BioDatabase
 
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith('_')]
--- bob/bio/spear/database/nist_sre08.py
+++ bob/bio/spear/database/nist_sre08.py
@@ -21,8 +21,7 @@ class NistSre08BioFile(AudioBioFile):
         Initializes this File object with an File equivalent from the underlying SQl-based interface for
         NIST SRE 2008 database.
         """
-#        super(NistSre08BioFile, self).__init__(client_id=f.client_id, path=f.path, file_id=f.id)
-        super(NistSre08BioFile, self).__init__(client_id='M_ID_X', path=f.path, file_id=f.id)
+        super(NistSre08BioFile, self).__init__(client_id=f.client_id, path=f.path, file_id=f.id)
 
         self.__f = f
         self.path = os.path.join( os.path.dirname(self.__f.path), self.__f.id)
@@ -60,11 +59,13 @@ class NistSre08BioDatabase(BioDatabase):
 
     def __init__(
             self,
+            protocol='short2-short3-all',
             **kwargs
     ):
         # call base class constructors to open a session to the database
         super(NistSre08BioDatabase, self).__init__(
             name='nist_sre08',
+            protocol=protocol,
             **kwargs)
 
 
@@ -81,3 +82,77 @@ class NistSre08BioDatabase(BioDatabase):
         groups = ['eval']
         retval = self.__db.objects(groups=groups, protocol=protocol, purposes=purposes, model_ids=model_ids, **kwargs)
         return [NistSre08BioFile(f) for f in retval]
+
+    def arrange_by_client(self, files):
+        """arrange_by_client(files) -> files_by_client
+
+        Arranges the given list of files by client id.
+        This function returns a list of lists of File's.
+
+        **Parameters:**
+
+        files : :py:class:`BioFile`
+          A list of files that should be split up by `BioFile.client_id`.
+
+        **Returns:**
+
+        files_by_client : [[:py:class:`BioFile`]]
+          The list of lists of files, where each sub-list groups the files with the same `BioFile.client_id`
+        """
+        client_files = {}
+        for f in files:
+            # we do not include files with unknown client_id
+            if f.client_id=='C_ID_X':
+              continue
+            if f.client_id not in client_files:
+                client_files[f.client_id] = []
+            client_files[f.client_id].append(f)
+        
+
+        files_by_clients = []
+        for client in sorted(client_files.keys()):
+            if client == 'C_ID_X':
+              continue
+            files_by_clients.append(client_files[client])
+        return files_by_clients
+
+
+    def training_files(self, step=None, arrange_by_client=False):
+        """training_files(step = None, arrange_by_client = False) -> files
+
+        Returns all training files for the given step, and arranges them by client, if desired, respecting the current protocol.
+        The files for the steps can be limited using the ``..._training_options`` defined in the constructor.
+
+        **Parameters:**
+
+        step : one of ``('train_extractor', 'train_projector', 'train_enroller')`` or ``None``
+          The step for which the training data should be returned.
+
+        arrange_by_client : bool
+          Should the training files be arranged by client?
+          If set to ``True``, training files will be returned in [[:py:class:`bob.bio.base.database.BioFile`]], where each sub-list contains the files of a single client.
+          Otherwise, all files will be stored in a simple [:py:class:`bob.bio.base.database.BioFile`].
+
+        **Returns:**
+
+        files : [:py:class:`BioFile`] or [[:py:class:`BioFile`]]
+          The (arranged) list of files used for the training of the given step.
+        """
+
+        if step is None:
+            training_options = self.all_files_options
+        elif step == 'train_extractor':
+            training_options = self.extractor_training_options
+        elif step == 'train_projector':
+            training_options = self.projector_training_options
+        elif step == 'train_enroller':
+            training_options = self.enroller_training_options
+        else:
+            raise ValueError(
+                "The given step '%s' must be one of ('train_extractor', 'train_projector', 'train_enroller')" % step)
+
+        files = self.sort(self.objects(protocol=self.protocol, groups='eval', **training_options))
+        if arrange_by_client:
+            return self.arrange_by_client(files)
+        else:
+            return files
