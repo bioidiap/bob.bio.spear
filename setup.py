b7fed7eff9e8e782714d13cbfa075cf05a72e9a9
Pavel Korshunov <pavel.korshunov@idiap.ch>
2016-11-20 02:01:14 +0100

    Added spqg replay database

--- setup.py
+++ setup.py
@@ -111,6 +111,8 @@ setup(
         'asvspoof-spoof             = bob.bio.spear.config.database.asvspoof:database_spoof',
         'voicepa-licit             = bob.bio.spear.config.database.voicepa:database_licit',
         'voicepa-spoof             = bob.bio.spear.config.database.voicepa:database_spoof',
+        'cpqd_replay-licit             = bob.bio.spear.config.database.cpqd_replay:database_licit',
+        'cpqd_replay-spoof             = bob.bio.spear.config.database.cpqd_replay:database_spoof',
       ],
 
       'bob.bio.preprocessor': [
