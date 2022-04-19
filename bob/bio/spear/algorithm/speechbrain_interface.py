f7900f3f0f0881f73f3d752deec9ea166ae5dbd5
Yannick DAYER <yannick.dayer@idiap.ch>
2022-04-19 17:03:31 +0200

    Reduce the scores in the scoring function directly
    the score function returns one score per model-probe pair.
    Fixes the problem of non-homogeneous score shape.

--- bob/bio/spear/algorithm/speechbrain_interface.py
+++ bob/bio/spear/algorithm/speechbrain_interface.py
@@ -1,5 +1,6 @@
 import logging
 
+import numpy as np
 import torch
 
 # Download pretrained SpeakerRecognition from SpeechBrain
@@ -20,15 +21,20 @@ class SpeechBrainInterface(BioAlgorithm):
         )
 
     def enroll(self, data):
-        return data
+        return data  # Defines the model as the data itself (used during the scoring)
 
     def score(self, model, data):
+        # Compute the score on each audio file of the model
+        scores = [
+            self.verification.verify_batch(
+                torch.from_numpy(m),
+                torch.from_numpy(data),
+            )[
+                0
+            ]  # Retrieves the score, discards the decision
+            for m in model  # Scores against each sample of model
+        ]
+        scores = np.array(scores, dtype=float)
 
-        scores = []
-        for _model in model:
-            score, _ = self.verification.verify_batch(
-                torch.from_numpy(_model), torch.from_numpy(data)
-            )
-            scores.append(score)
-
-        return scores
+        # Reduce to one score for that probe on this model
+        return [scores.mean()]
