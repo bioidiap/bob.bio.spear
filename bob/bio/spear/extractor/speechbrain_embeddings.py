c7d1be73aa89e866cfc9b5d5aedf5e5a3d336e71
Yannick DAYER <yannick.dayer@idiap.ch>
2022-04-27 11:46:08 +0200

    speechbrain as embeddings transformer

---
+++ bob/bio/spear/extractor/speechbrain_embeddings.py
@@ -0,0 +1,38 @@
+import numpy as np
+import torch
+from sklearn.base import BaseEstimator
+from speechbrain.pretrained import EncoderClassifier
+
+
+class SpeechbrainEmbeddings(BaseEstimator):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.verification = EncoderClassifier.from_hparams(
+            source="speechbrain/spkrec-ecapa-voxceleb",
+            savedir="pretrained_models/spkrec-ecapa-voxceleb",
+        )
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, audio_tracks, y=None):
+        audio_lengths = [len(a) for a in audio_tracks]
+
+        audio_stack = np.vstack(
+            [
+                np.pad(a, (0, max(audio_lengths) - len(a)), "constant")
+                for a in audio_tracks
+            ]
+        )
+        relative_lengths = [length / max(audio_lengths) for length in audio_lengths]
+
+        embeddings = self.verification.encode_batch(
+            torch.from_numpy(audio_stack),
+            torch.tensor(relative_lengths),
+            normalize=True,
+        )
+
+        return np.array(
+            embeddings, dtype=float
+        )  # Embeddings corresponding to one model
