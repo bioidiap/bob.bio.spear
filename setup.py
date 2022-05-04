a4b99e7d67abed86c10ea736cf08872833552c9c
Flavio Tarsetti <flavio.tarsetti@idiap.ch>
2022-05-04 11:53:04 +0200

    Change speechbrain pipeline to ecapa-voxceleb

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ setup(
         "bob.bio.pipeline": [
             "gmm-voxforge  = bob.bio.spear.config.pipeline.mfcc60_gmm_voxforge:pipeline",
             "isv-voxforge  = bob.bio.spear.config.pipeline.mfcc60_isv_voxforge:pipeline",
-            "speechbrain-pipeline   = bob.bio.spear.config.pipeline.speechbrain_pipeline:pipeline",  # Do not call it `speechbrain` as it would try to load the speechbrain module instead
+            "speechbrain-ecapa-voxceleb   = bob.bio.spear.config.pipeline.speechbrain_ecapa_voxceleb:pipeline",  # Do not call it `speechbrain` as it would try to load the speechbrain module instead
         ],
         "bob.bio.config": [
             # databases:
