29dc6183837ab231592e5a3ef891df37d1f1d0dd
Yannick DAYER <yannick.dayer@idiap.ch>
2022-06-13 17:54:13 +0200

    [doc] update the config description of nist-sre.

--- bob/bio/spear/config/database/nist_sre04to16.py
+++ bob/bio/spear/config/database/nist_sre04to16.py
@@ -4,19 +4,39 @@
 
 """Default configuration file for the NIST-SRE database.
 
-Use this file (defined as ``nist-sre`` in ``entry-points``) as parameter to the
+Use this file (defined as ``nist-sre04to16`` in ``entry-points``) as parameter to the
 ``bob bio pipeline`` commands::
 
-    $ bob bio pipeline simple -d nist-sre [...]
+    $ bob bio pipeline simple nist-sre04to16 [...]
 
-Each of those three protocols have the same dev and eval groups. The differences are in
-**the training set**:
+This database uses the NIST-SRE 2016 to evaluate the performance of a system. Other
+datasets from NIST are used as training data.
+
+Three protocols are available. They have the same dev and eval groups. The differences
+are in the training set:
     - core: Selected set of files from the NIST-SRE database with labels.
     - core-aug: Augmented data with reverberation or background noise (music, noise,
         speech/babble).
-    - unlabeled: Different data that were not labeled.
+    - unlabeled: Includes data that were not labeled.
 
-To get the augmented data, please use the correct pipeline configuration. # TODO specify which one
+
+Bob does not provide the data files for its databases.
+After downloading the NIST-SRE database files, configure their location using::
+
+    $ bob config set bob.db.nist_sre04to16.directory /path/to/nist-sre-root/
+
+This folder (pointed to by the ``bob config`` command) should contain the following
+subfolders:
+    - ``nist_sre/SRE16``: used for dev and eval groups for enrollment and scoring.
+    - ``Switchboard_2_Phase_II/dbase``: used for training.
+    - ``Switchboard_2_Phase_III/dbase``: used for training.
+    - ``Switchboard_Cellular_Part1/dbase``: used for training.
+    - ``Switchboard_Cellular_Part2/dbase``: used for training.
+    - ``nist_sre/SRE04``: used for training.
+    - ``nist_sre/SRE05``: used for training.
+    - ``nist_sre/SRE06``: used for training.
+    - ``nist_sre/SRE08``: used for training.
+    - ``nist_sre/SRE10``: used for training.
 """
 
 from bob.bio.spear.database import SpearBioDatabase
