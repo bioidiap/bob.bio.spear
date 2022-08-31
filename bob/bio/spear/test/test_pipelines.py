4b40153831d8dade0d199a5a81ebcf49e3086b43
Yannick DAYER <yannick.dayer@idiap.ch>
2022-08-31 19:04:49 +0200

    [tests] Remove skips

--- bob/bio/spear/test/test_pipelines.py
+++ bob/bio/spear/test/test_pipelines.py
@@ -5,11 +5,11 @@ from sklearn.pipeline import Pipeline as SklearnPipeline
 from bob.bio.base import load_resource
 from bob.bio.base.algorithm import GMM
 from bob.bio.base.pipelines import PipelineSimple
-from bob.bio.base.test.utils import is_library_available
-from bob.learn.em import IVectorMachine
+
+# from bob.bio.base.test.utils import is_library_available
 
 
-@is_library_available("torchaudio")
+# @is_library_available("torchaudio")
 def test_mfcc_gmm_voxforge():
     """Creating the gmm-voxforge pipeline."""
     pipeline = load_resource("gmm-voxforge", "pipeline")
@@ -28,7 +28,7 @@ def test_mfcc_gmm_mobio():
     assert isinstance(pipeline.biometric_algorithm, GMM)
 
 
-@is_library_available("torchaudio")
+# @is_library_available("torchaudio")
 def test_mfcc_isv_voxforge():
     """Creating the isv-voxforge pipeline."""
     pipeline = load_resource("isv-voxforge", "pipeline")
@@ -36,12 +36,9 @@ def test_mfcc_isv_voxforge():
     assert isinstance(pipeline.transformer, SklearnPipeline)
 
 
-@is_library_available("torchaudio")
+# @is_library_available("torchaudio")
 def test_mfcc_ivector():
     """Creating the IVector pipeline."""
     pipeline = load_resource("ivector-default", "pipeline")
     assert isinstance(pipeline, PipelineSimple)
     assert isinstance(pipeline.transformer, SklearnPipeline)
-    assert isinstance(
-        pipeline.transformer.steps[-1][1].estimator, IVectorMachine
-    )
